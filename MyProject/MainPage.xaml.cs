592bbfe652e95642f3bfedc2f85ee5e3c09eebd7
Jakob Doepping <jdoepping@hotmail.com>
2024-08-14 10:05:55 +0200

    added images

--- MyProject/MainPage.xaml.cs
+++ MyProject/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using static MyProject.MainPage;
 
 namespace MyProject
 {
@@ -19,5 +20,50 @@ namespace MyProject
         }
 
         public string Text01 => Properties.Resource.Text01;
+        public string HeartsImage => ImageResourcesEnum.Hearts.ToFileName();
+        public enum ImageResourcesEnum
+        {
+            Transparent,
+            Hearts,
+            Clubs,
+            Spades,
+            Diamonds,
+            NoTrumph
+        }
+
+       
+
     }
+    public static class ImageResourcesExtensions
+    {
+        public static string ToFileName(this ImageResourcesEnum enumResource)
+        {
+            var resource = string.Empty;
+            switch (enumResource)
+            {
+                case ImageResourcesEnum.Transparent:
+                    resource = "transparent";
+                    break;
+                case ImageResourcesEnum.Hearts:
+                    resource = "hjerter";
+                    break;
+                case ImageResourcesEnum.Clubs:
+                    resource = "kloer_green";
+                    break;
+                case ImageResourcesEnum.Spades:
+                    resource = "spar";
+                    break;
+                case ImageResourcesEnum.Diamonds:
+                    resource = "ruder_orange";
+                    break;
+                case ImageResourcesEnum.NoTrumph:
+                    resource = "trumf";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(resource), resource, null);
+            }
+            return $"../../../../../images/{resource}.png";
+        }
+    }
+
 }
